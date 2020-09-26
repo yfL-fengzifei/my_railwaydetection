6d098f0d554cd6766a4f880d903b2a93f55047bf
yfL-fengzifei <yunfei_li@tju.edu.cn>
2020-09-26 22:57:01 +0800

    update

--- line.py
+++ line.py
@@ -34,21 +34,24 @@ def find_line(bird_img):
 
     #找到直方图左右部分的峰值
     #these will be the strating point for the left and right liines
+    #把直方图分成两部分
     midpoint=np.int(histogram.shape[0]/2)
+    #分别返回左右两部分中最大值所在的索引(列索引)
     leftx_base=np.argmax(histogram[:midpoint])
     rightx_base=np.argmax(histogram[midpoint:])+midpoint #argmax返回的是索引
 
     #选择滑窗的数量
+    #根据参考代码，其中应该是沿第一维度(列),即图像的高方向分成几个窗口
     nwindows=9
     #设置窗口的高度
     window_height=np.int(bird_img.shape[0]/nwindows)
     #identify the x and y positions of all nonzero pixels in the image
-
     nonzero=bird_img.nonzero() #返回的是非0值的索引
     nonzeroy=np.array(nonzero[0])
     nonzeroyx=np.array(nonzero[1])
 
     #current positions to be based updated for each window
+    #把当前 左右直方图中最大最值的索引取出来
     leftx_current=leftx_base
     rightx_current=rightx_base
 
@@ -64,7 +67,20 @@ def find_line(bird_img):
 
     #一个一个的遍历窗口
     for window in range(nwindows):
-        pass
+        #窗口边界
+        #窗口的上边界
+        win_y_low=bird_img.shape[0]-(window+1)*window_height #虽然是low 但其实是上顶边，因为数小
+        #窗口的下边界
+        win_y_high=bird_img.shape[0]-window*window_height #虽然是high 但其实是下底边，因为数大
+        #当前最大(列索引下)
+        win_xleft_low=leftx_current-margin
+        win_xleft_high=leftx_current+margin
+        win_xright_low=rightx_current-margin
+        win_xright_high=rightx_current+margin
+
+        #
+
+
 
 
 
